42f5844890d763c9271fcf3777c3622e1017b644
Mauro Passerino <mpasserino@irobot.com>
2023-12-22 10:47:05 -0300

    Modify example to showcase cli/srv double-delivery issue

--- rclcpp/services/minimal_service/main.cpp
+++ rclcpp/services/minimal_service/main.cpp
@@ -1,45 +1,91 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#include <cinttypes>
 #include <memory>
-
-#include "example_interfaces/srv/add_two_ints.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/srv/big_srv.hpp"
+#include "rclcpp/experimental/executors/events_executor/events_executor.hpp"
 
-using AddTwoInts = example_interfaces::srv::AddTwoInts;
-rclcpp::Node::SharedPtr g_node = nullptr;
+#include <string>
 
-void handle_service(
-  const std::shared_ptr<rmw_request_id_t> request_header,
-  const std::shared_ptr<AddTwoInts::Request> request,
-  const std::shared_ptr<AddTwoInts::Response> response)
-{
-  (void)request_header;
-  RCLCPP_INFO(
-    g_node->get_logger(),
-    "request: %" PRId64 " + %" PRId64, request->a, request->b);
-  response->sum = request->a + request->b;
-}
+using namespace std::chrono;
+using BigSrv = example_interfaces::srv::BigSrv;
+using rclcpp::experimental::executors::EventsExecutor;
+
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
-  g_node = rclcpp::Node::make_shared("minimal_service");
-  auto server = g_node->create_service<AddTwoInts>("add_two_ints", handle_service);
-  rclcpp::spin(g_node);
+
+  // Node options
+  rclcpp::NodeOptions node_options = rclcpp::NodeOptions();
+  node_options.use_intra_process_comms(false);
+  node_options.start_parameter_services(false);
+  node_options.start_parameter_event_publisher(false);
+
+  // Create executors
+  auto service_executor = std::make_shared<rclcpp::experimental::executors::EventsExecutor>();
+  auto client_executor = std::make_shared<rclcpp::experimental::executors::EventsExecutor>();
+
+  // Create a node for the service
+  auto server_node = rclcpp::Node::make_shared("server_node", node_options);
+  service_executor->add_node(server_node->get_node_base_interface());
+
+  // Create service
+  auto m_service = rclcpp::create_service<BigSrv>(
+      server_node->get_node_base_interface(),
+      server_node->get_node_services_interface(),
+      "example_service",
+      [server_node](const std::shared_ptr<BigSrv::Request> request,
+          const std::shared_ptr<BigSrv::Response> response)
+      {
+          static unsigned int serial_num = 1;
+          RCLCPP_INFO(server_node->get_logger(), "Received service client request... Sending response: %d", serial_num);
+          response->success = true;
+          response->message = std::to_string(serial_num++);
+          response->big_data_response = request->big_data_request;
+      },
+      rclcpp::ServicesQoS().get_rmw_qos_profile(),
+      nullptr);
+
+  // Create nodes for the clients
+  auto client_node = rclcpp::Node::make_shared("client_node", node_options);
+  auto client_node2 = rclcpp::Node::make_shared("client_node2", node_options);
+  client_executor->add_node(client_node);
+  client_executor->add_node(client_node2);
+
+  // Create 2 clients with same service name
+  auto m_client = client_node->create_client<BigSrv>("example_service");
+  auto m_client_2 = client_node2->create_client<BigSrv>("example_service");
+
+  // Spin the executors
+  auto service_spin_thread = std::thread(std::bind([&](){ service_executor->spin(); }));
+  auto client_spin_thread = std::thread(std::bind([&](){ client_executor->spin(); }));
+
+  // Wait for service
+  while (!m_client->wait_for_service(1s)) {
+      if (!rclcpp::ok()) {
+        RCLCPP_ERROR(client_node->get_logger(), "Interrupted while waiting for the service. Exiting.");
+        return 0;
+      }
+      RCLCPP_INFO(client_node->get_logger(), "service not available, waiting again...");
+  }
+
+  // Make client requests in a loop many times
+  uint counter = 1;
+  do {
+      auto request = std::make_shared<BigSrv::Request>();
+      request->data = true;
+      request->big_data_request[1000] = 3;
+
+      RCLCPP_INFO(client_node->get_logger(), "Sending request: %d" , counter);
+      auto result_future = m_client->async_send_request(request);
+
+      RCLCPP_INFO(client_node->get_logger(), "Waiting for response: %d" , counter);
+
+      auto answer = result_future.get();
+      RCLCPP_INFO(client_node->get_logger(), "Got response: %d", answer->big_data_response[1000]);
+  }while(++counter<10);
+
   rclcpp::shutdown();
-  g_node = nullptr;
+  service_spin_thread.join();
+  client_spin_thread.join();
   return 0;
 }
